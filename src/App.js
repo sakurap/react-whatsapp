991c5691b066463d1083be06801de79bb20b7305
Siramest <siramest@gmail.com>
2021-10-14 15:35:23 -0400

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./App.css";
 import useWindowSize from "./hooks/useWindowSize";
+import Login from "./components/Login";
+import useAuthUser from "./hooks/useAuthUser";
 
 export default function App() {
   const page = useWindowSize();
+  // const [user] = useAuthState(auth);
+  const user = useAuthUser();
+
+  // if (!user) {
+  //   return <Login />;
+  // }
+
+  // console.log({user});
 
   return (
     <div className="app" style={{ ...page }}>
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
-import filrebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/database';
 
 const firebaseConfig = {
     apiKey: "AIzaSyD_krTi9SStqjNMG57iilZW9grvChDf3Bk",
@@ -12,4 +14,22 @@ const firebaseConfig = {
   };
 
   // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage().ref('images');
+const audioStorage = firebase.storage().ref('audios');
+const createTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+const serverTimestamp = firebase.database.ServerValue.TIMESTAMP;
+
+export {
+    db,
+    auth,
+    provider,
+    storage,
+    audioStorage,
+    createTimestamp,
+    serverTimestamp
+}
