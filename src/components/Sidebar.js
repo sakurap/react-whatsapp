a8fa64bbe2a8599073ebef508e049cb9559938a8
Siramest <siramest@gmail.com>
2021-10-20 13:14:54 -0400

    Create Sidebar menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import "./Sidebar.css";
-import { Add, ExitToApp, SearchOutlined } from "@material-ui/icons";
+import SidebarList from './SidebarList';
+import { Add, ExitToApp, Home, Message, PeopleAlt, SearchOutlined } from "@material-ui/icons";
 import { auth } from "../firebase";
+import { NavLink, Switch, Route } from "react-router-dom";
 
 export default function Sidebar({ user, page }) {
+  const [menu, setMenu] = React.useState(1);
   function signOut(){
     auth.signOut();
   }
+
+  let Nav;
+  if (page.isMobile) {
+    Nav = NavLink;
+  } else {
+    Nav = props => (
+      <div 
+        className={`${props.activeClass ? "sidebar__menu--selected" : ""}`}
+        onClick={props.onClick}
+      >
+        {props.children}
+      </div>
+    )
+  }
+
   return <div className="sidebar" style={{
     minHeight: page.isMobile ? page.height : "auto", 
   }} >
@@ -35,6 +53,67 @@ export default function Sidebar({ user, page }) {
         />
       </form>
     </div>
+
+    <div className="sidebar__menu">
+      <Nav
+        to="/chats"
+        activeClassName="sidebar__menu--selected"
+        onClick={() => setMenu(1)}
+        activeClass={menu === 1}
+      >
+        <div className="sidebar_menu--home">
+          <Home />
+          <div className="sidebar__menu--line" />
+        </div>
+      </Nav>
+      <Nav
+        to="/rooms"
+        activeClassName="sidebar__menu--selected"
+        onClick={() => setMenu(2)}
+        activeClass={menu === 2}
+      >
+        <div className="sidebar_menu--rooms">
+          <Message />
+          <div className="sidebar__menu--line" />
+        </div>
+      </Nav>
+      <Nav
+        to="/users"
+        activeClassName="sidebar__menu--selected"
+        onClick={() => setMenu(3)}
+        activeClass={menu === 3}
+      >
+        <div className="sidebar_menu--users">
+          <PeopleAlt />
+          <div className="sidebar__menu--line" />
+        </div>
+      </Nav>
+    </div>
+
+    {page.isMobile ? (
+      <Switch>
+        <Route path="/chats">
+          <SidebarList />
+        </Route>
+        <Route path="/rooms">
+          <SidebarList />
+        </Route>
+        <Route path="/users">
+          <SidebarList />
+        </Route>
+        <Route path="/search">
+          <SidebarList />
+        </Route>
+      </Switch>
+    ) : menu === 1 ? (
+      <SidebarList />
+    ) : menu === 2 ? (
+      <SidebarList />
+    ) : menu === 3 ? (
+      <SidebarList />
+    ) : menu === 4 ? (
+      <SidebarList />
+    ) : null}
 
     <div className="sidebar__chat--addRoom">
       <IconButton>
