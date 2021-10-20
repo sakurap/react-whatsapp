d946628f48ca1976ffcd2609b40c6b25f947c5da
Siramest <siramest@gmail.com>
2021-10-20 11:23:37 -0400

    Add Search

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import "./Sidebar.css";
-import { ExitToApp } from "@material-ui/icons";
+import { Add, ExitToApp, SearchOutlined } from "@material-ui/icons";
 import { auth } from "../firebase";
 
 export default function Sidebar({ user, page }) {
@@ -21,6 +21,25 @@ export default function Sidebar({ user, page }) {
           <ExitToApp />
         </IconButton>
       </div>
+    
+    
+    
+    </div>
+    <div className="sidebar__search">
+      <form className="sidebar__search--container">
+        <SearchOutlined />
+        <input 
+          placeholder="Search for users or rooms"
+          type="text"
+          id="search"
+        />
+      </form>
+    </div>
+
+    <div className="sidebar__chat--addRoom">
+      <IconButton>
+        <Add />
+      </IconButton>
     </div>
   </div>;
 }
