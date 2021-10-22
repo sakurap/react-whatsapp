901e5f4f328ff2ea796c21b90509ce4d2e934500
Siramest <siramest@gmail.com>
2021-10-22 11:35:13 -0400

    Getting room data

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useParams } from "react-router";
+import useRoom from "../hooks/useRoom";
 import "./Chat.css";
 
-export default function Chat() {
+export default function Chat({ user, page}) {
+  const { roomId } = useParams()
+
+  const room =  useRoom(roomId, user.uid);
+
+  console.log({room});
+
   return <div className="chat">Chat</div>;
 }
